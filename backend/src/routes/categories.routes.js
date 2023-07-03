a05224cdaad85c1e664d55047f847f0b1a678271
fxckcode <diegoale8582@hotmail.com>
2023-07-03 12:00:00 -0500

    add tryCatch to backend

--- backend/src/routes/categories.routes.js
+++ backend/src/routes/categories.routes.js
@@ -4,30 +4,45 @@ import { prisma } from "../db.js";
 const router = Router();
 
 router.get("/categories", async (req, res) => {
-    const categories = await prisma.category.findMany();
-    res.json(categories);
+    try {
+        const categories = await prisma.category.findMany();
+        res.json(categories);
+    } catch (error) {
+        console.error("Error show categories", error);
+        res.status(500).json({error: "Error show categories"})
+    }
 });
 
 router.post("/categories", async (req, res) => {
-    const category = await prisma.category.create({
-        data: {
-            name: req.body.name
-        } 
-    })
-
-    res.json(category)
+    try {
+        const category = await prisma.category.create({
+            data: {
+                name: req.body.name
+            } 
+        })
+    
+        res.json(category)
+    } catch (error) {
+        console.error("Error create categorie", error);
+        res.status(500).json({error: "Error create categorie"})
+    }
 
 });
 
 router.delete("/categories/:id", async (req, res) => {
-    const { id } = req.params;
-    const category = await prisma.category.delete({
-        where: {
-            id: parseInt(id)
-        }
-    })
-
-    res.json(category)
+    try {
+        const { id } = req.params;
+        const category = await prisma.category.delete({
+            where: {
+                id: parseInt(id)
+            }
+        })
+    
+        res.json(category)
+    } catch (error) {
+        console.error("Error delete categorie", error);
+        res.status(500).json({error: "Error delete categorie"})
+    }   
 });
 
 export default router;
--- backend/src/routes/tasks.routes.js
+++ backend/src/routes/tasks.routes.js
@@ -4,88 +4,123 @@ import { prisma } from "../db.js";
 const router = Router();
 
 router.get("/tasks", async (req, res) => {
-    const tasks = await prisma.tasks.findMany({
-        include: {
-            category: true
-        },
-        where: {
-            status: false
-        }
-    });
-    res.json(tasks)
+    try {
+        const tasks = await prisma.tasks.findMany({
+            include: {
+                category: true
+            },
+            where: {
+                status: false
+            }
+        });
+        res.json(tasks)
+    } catch (error) {
+        console.error("Error show tasks", error);
+        res.status(500).json({error: "Error show tasks"})
+    }
 });
 
 router.get("/completed", async (req, res) => {
-    const tasks = await prisma.tasks.findMany({
-        where: {
-            status: true
-        },
-        include: {
-            category: true
-        }
-    })
-    res.json(tasks)
+    try {
+        const tasks = await prisma.tasks.findMany({
+            where: {
+                status: true
+            },
+            include: {
+                category: true
+            }
+        })
+        res.json(tasks)
+    } catch (error) {
+        console.error("Error show tasks completed", error);
+        res.status(500).json({error: "Error show tasks completed"})
+    }
 })
 
 router.put("/checktask/:id", async (req, res) => {
-    const {id} = req.params
-    await prisma.tasks.update({
-        data: {
-            status: true
-        }, where: {
-            id: parseInt(id)
-        }
-    })
-    res.json(true)
+    try {
+        const {id} = req.params
+        await prisma.tasks.update({
+            data: {
+                status: true
+            }, where: {
+                id: parseInt(id)
+            }
+        })
+        res.json(true)
+    } catch (error) {
+        console.error("Error update status task", error);
+        res.status(500).json({error: "Error update status task"})
+    }
 })
 
 router.get("/task/:id", async (req, res) => {
-    const {id} = req.params
-    const showTask = await prisma.tasks.findFirst({
-        where: {
-            id: parseInt(id)
-        }, include: {
-            category: true
-        }
-    })
-    res.json(showTask)
+    try {
+        const {id} = req.params
+        const showTask = await prisma.tasks.findFirst({
+            where: {
+                id: parseInt(id)
+            }, include: {
+                category: true
+            }
+        })
+        res.json(showTask)
+    } catch (error) {
+        console.error("Error show task", error);
+        res.status(500).json({error: "Error show task"})
+    }
 })
 
 router.post("/tasks", async (req, res) => {
-    const task = await prisma.tasks.create({
-        data: {
-            title: req.body.title,
-            description: req.body.description,
-            categoryId: parseInt(req.body.categoryId)
-        } 
-    })
-    res.json(task)
+    try {
+        const task = await prisma.tasks.create({
+            data: {
+                title: req.body.title,
+                description: req.body.description,
+                categoryId: parseInt(req.body.categoryId)
+            } 
+        })
+        res.json(task)
+    } catch (error) {
+        console.error("Error create task", error);
+        res.status(500).json({error: "Error create task"})
+    }
 })
 
 router.put("/task/:id", async (req, res) => {
-    const {id} = req.params
-    const update_task = await prisma.tasks.update({
-        where: {
-            id: parseInt(id)
-        },
-        data: {
-            title: req.body.title,
-            description: req.body.description,
-            categoryId: parseInt(req.body.categoryId)
-        }
-    });
-    res.json(update_task)
+    try {
+        const {id} = req.params
+        const update_task = await prisma.tasks.update({
+            where: {
+                id: parseInt(id)
+            },
+            data: {
+                title: req.body.title,
+                description: req.body.description,
+                categoryId: parseInt(req.body.categoryId)
+            }
+        });
+        res.json(update_task)
+    } catch (error) {
+        console.error("Error update task", error);
+        res.status(500).json({error: "Error update task"})
+    }
 })
 
 router.delete("/tasks/:id", async (req, res) => {
-    const {id} = req.params
-    const deleted_task = await prisma.tasks.delete({
-        where: {
-            id: parseInt(id)
-        }
-    })
-
-    res.json(deleted_task)
+    try {
+        const {id} = req.params
+        const deleted_task = await prisma.tasks.delete({
+            where: {
+                id: parseInt(id)
+            }
+        })
+    
+        res.json(deleted_task)
+    } catch (error) {
+        console.error("Error delete task", error);
+        res.status(500).json({error: "Error delete task"})
+    }
 })
 
 export default router;
